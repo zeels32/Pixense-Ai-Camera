1aec47d6014c7f4cc251b1d8834cedc28950e4a8
Zeel <solankizeel91@gmail.com>
2026-09-14 20:43:01 +0530

    feat: update version code and version name to 7 and 1.3 respectively

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.pixense.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.2"
+        versionCode = 7
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
